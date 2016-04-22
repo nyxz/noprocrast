289fb0ec76e972f11a66db7e95c4d40ddbd4f1f9
nyxz <master033@gmail.com>
2016-04-22 22:43:04 +0300

    Initialized the grid.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
+'use strict'
+
+$(document).ready(function() {
+
+    var years = 80;
+    var weeks = 52;
+    var totalWeeks = years * weeks;
+    for (var i = 0; i < totalWeeks; i++) {
+        $('#grid').append('<div class="box"></div>');
+    }
+
+    var currentWeek = 1246;
+    $('#grid .box').each(function(i, val) {
+        if (i == currentWeek) {
+            return false;
+        } else {
+            $(val).addClass('active');
+        }
+    });
+
+
+    $('#dp1').fdatepicker({
+        initialDate: '02-12-1989',
+        format: 'mm-dd-yyyy',
+        disableDblClickSelection: true
+    });
+});
+
